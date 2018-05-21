f238fa54043d06d7c2245f832eaf9bc1d12078e1
Mike <mike@8327.co.za>
2018-05-21 16:16:04 +0200

    added styling to tracker

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -1,9 +1,9 @@
 import { Task } from './task-template';
 
 export const TASKS: Task[] = [
-    { id: 11, name: 'Task Tracker 1', summary: 'Build TaskTracker Site' },
-    { id: 12, name: 'Task Tracker 2', summary: 'Build TaskTracker functionality' },
-    { id: 13, name: 'Task Tracker 3', summary: 'Connect API' },
-    { id: 14, name: 'Task Tracker 4', summary: 'Build CRUD Frunctionality' },
-    { id: 15, name: 'Task Tracker 5', summary: 'Style to look badass' },
+    { id: 1, name: 'Build Site', summary: 'Build TaskTracker Site' },
+    { id: 2, name: 'Build Functionality', summary: 'Build TaskTracker functionality' },
+    { id: 3, name: 'API', summary: 'Connect API' },
+    { id: 4, name: 'CRUD functionality', summary: 'Build CRUD Frunctionality' },
+    { id: 5, name: 'Style', summary: 'Style to look badass' },
 ];
